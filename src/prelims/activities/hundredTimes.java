ee3d95a71e5d85a37f0df5254488052ce422f96d
loudi <tffdiamond@gmail.com>
2022-09-21 12:30:45 +0800

    schoolWorks

---
+++ src/prelims/activities/hundredTimes.java
@@ -0,0 +1,17 @@
+package prelims.activities;
+
+public class hundredTimes {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            System.out.println("Lou Diamond T. Morados");
+            i++;
+        }
+        do {
+            System.out.println("Lou Diamond T. Morados");
+            i++;
+        }
+        while (i < 100);
+        for (i=0; i<100; i++) System.out.println("Lou Diamond T. Morados");
+    }
+}
